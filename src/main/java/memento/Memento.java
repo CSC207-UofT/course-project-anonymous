51c4e64030cf12d7b824707b7e04e075c9455797
keviny15 <kevinnyyy@gmail.com>
2021-11-08 23:56:39 -0500

    memento example

--- src/main/java/memento/Memento.java
+++ src/main/java/memento/Memento.java
@@ -1,6 +1,24 @@
 package main.java.memento;
 
 public class Memento {
-    private
 
+    private Flight flight;
+
+    public Memento (Flight flight) {
+        super();
+        this.flight = flight;
+    }
+
+    public Flight getFlight() {
+        return fLight;
+    }
+
+    public void setFlight (Flight flight) {
+        this.flight = flight;
+    }
+
+    public Flight finalreturn() {
+        return flight;
+
+    }
 }
--- src/main/java/memento/Originator.java
+++ src/main/java/memento/Originator.java
@@ -1,4 +1,32 @@
 package main.java.memento;
 
+
 public class Originator {
+
+    Flight flight;
+
+    public Flight getFlight(){
+        return flight;
+    }
+
+    public void setFlight (Flight flight) {
+        this.flight = flight;
+    }
+
+    public Memento makeMemento(){
+        return new Memento(flight);
+    }
+
+    public void setMemento (Memento memento) {
+
+        flight = memento.getFlight() ;
+    }
+
+    @Override
+    public String toString() {
+
+        return Originator [flight =];
+
+    }
+
 }

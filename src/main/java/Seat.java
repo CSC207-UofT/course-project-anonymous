11e93fcac5f043de7ecd7a63f763b47b07f30d6b
dinkarverma02 <dinkarverma2002@gmail.com>
2021-11-10 15:09:44 -0500

    created SeatFactory entity class created SeatManager use case class shifted functions from Seat to SeatManager shifted functions from SeatFactory to SeatManager created getSeat function in SeatFactory

--- src/main/java/Seat.java
+++ src/main/java/Seat.java
@@ -2,20 +2,19 @@ public abstract class Seat {
     // TODO delete baggageallowance and refundable since they're redundant
     private int id;
     private double price;
-    // private boolean isOccupied;
-    // isOccupied takes values of "x" meaning occupied or "o" meaning available
-    private String isOccupied;
+    private boolean isOccupied;
 
     /**
      * Construct a Seat, giving it the given
-     * id and price.
+     * id and price. The occupied status of a Seat
+     * is set to false by default
      *
      * @param id The EconomySeat's id
      * @param price  The EconomySeat's price
      */
     public Seat(int id, double price) {
         this.id = id;
-        this.isOccupied = "o"; // "o" meaning available
+        this.isOccupied = false;
         this.price = price;
     }
 
@@ -35,22 +34,10 @@ public abstract class Seat {
         this.id = id;
     }
 
-//    public boolean isOccupied() {
-//        return isOccupied;
-//    }
-
     /**
-     * @return whether this seat is occupied or not
+     * @return the occupied status of this Seat
      */
-    public String getOccupiedSymbol() {
-        /*
-        Return the symbol of if it is ocupied or not
-         */
-//        if (this.isOccupied) {
-//            return "x";
-//        } else {
-//            return "o";
-//        }
+    public boolean getOccupied() {
         return this.isOccupied;
     }
 
@@ -58,10 +45,21 @@ public abstract class Seat {
      * Set the occupied status of this Seat
      * to the given occupied status
      *
-     * @param occupied     The Seat's occupied status
+     * @param isOccupied     The Seat's occupied status
      */
-    public void setOccupiedSymbol(String occupied) {
-        this.isOccupied = occupied;
+    public void setOccupied(boolean isOccupied) {
+        this.isOccupied = isOccupied;
+    }
+
+    /**
+     * @return the symbol of if this seat is occupied or not
+     */
+    public String getOccupiedSymbol() {
+        if (this.isOccupied) {
+            return "x";
+        } else {
+            return "o";
+        }
     }
 
     /**
@@ -95,46 +93,9 @@ public abstract class Seat {
     public abstract int numberOfCheckInBagsAllowed();
 
     /**
-     * Calculate the refund price for a given Seat based
-     * on how many days are left until departure date
-     *
-     * @param daysLeft how many days are left until the departure date
-     *
-     * @return the refunded price of the Seat
-     */
-    public double calculateRefundByDaysLeft(int daysLeft) {
-        // full refund if refund asked before 7 days of the flight
-        if (daysLeft >= 7) {
-            return 0;
-        } else if (daysLeft >= 1) {
-            // partial refund if asked before 1 day but
-            // after 7 days of the flight
-            return this.price - (this.price * daysLeft * 0.01);
-        } else {
-            return this.price;
-        }
-    }
-
-    /**
-     * Calculate the charge for changing the date of departure based
-     * on how many days are left until departure date
-     *
-     * @param daysLeft how many days are left until the departure date
-     *
-     * @return the charge price of making this change
-     */
-    //TODO implement this function
-    public double calculateDateChangeChargeByDateLeft(int daysLeft) {
-        return 0;
-    }
-
-    /**
      * @return which class the Seat represents
      */
     public String getSeatClass() {
-        //TODO maybe shift this to a use case?
-        //maybe better to keep it here since we won't have to define it in every use case class
-        // we use it in
         if (this instanceof EconomySeat) {
             return "Economy";
         } else if (this instanceof BusinessClassSeat) {

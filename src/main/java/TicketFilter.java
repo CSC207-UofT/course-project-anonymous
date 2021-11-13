aba7d1f506398fb2820b5bd063ac43575c207459
Avnish Pasari <avnish.pasari@mail.utoronto.ca>
2021-11-13 03:12:18 -0500

    Adding Comments to the code files

--- src/main/java/TicketFilter.java
+++ src/main/java/TicketFilter.java
@@ -2,14 +2,18 @@ import java.util.ArrayList;
 
 public class TicketFilter {
     ArrayList<Ticket> getTicketsForPassenger(TicketManager ticketManager, Passenger passenger) {
+
+        // Creating an ArrayList to store the tickets
         ArrayList<Ticket> tickets_so_far = new ArrayList<>();
 
+        // Returns all tickets booked by a single passenger
         for (Ticket ticket : ticketManager) {
             if (ticket.getPassenger().getId() == passenger.getId()) {
                 tickets_so_far.add(ticket);
             }
         }
 
+        // Returning an ArrayList of all tickets booked by a particular passenger
         return tickets_so_far;
     }
 }

5eb0660603270fa0aabf9774aafaa106f0b93759
Avnish Pasari <avnish.pasari@mail.utoronto.ca>
2021-11-13 13:47:28 -0500

    Adding documentation to test file

--- src/test/java/MealManager_test.java
+++ src/test/java/MealManager_test.java
@@ -18,10 +18,12 @@ public class MealManager_test {
 
         @Test
         public void testMeals() {
-
+            // Checking if no meals are available. This should always be vacously true
             assert(mealManager.checkForMeal("No Meal") == true);
+            // Testing that the system is correctly adding meals and storing it in the system
             mealManager.addMeal("Veg Sushi",10,11.11,true);
             assert(mealManager.checkForMeal("Veg Sushi") == true);
+            // Testing that the system correctly identifies that certain meals are not present in the system
             assert(mealManager.checkForMeal("Pizza") == false);
 
         }

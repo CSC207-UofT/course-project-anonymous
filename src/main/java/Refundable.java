3c72a80f262ca4c177d23ceb9bc5d1e185d16989
dinkarverma02 <dinkarverma2002@gmail.com>
2021-11-28 03:16:00 -0500

    .

--- src/main/java/Refundable.java
+++
@@ -1,4 +0,0 @@
-public interface Refundable {
-    public double calculateRefundByDaysLeft(double price, int daysLeft);
-    public double calculateDateChangeChargeByDateLeft(double price, int daysLeft);
-}

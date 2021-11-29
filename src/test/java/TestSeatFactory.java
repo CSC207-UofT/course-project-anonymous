182b24493944dd33589235eb6917d8e0c0c5637b
dinkarverma02 <dinkarverma2002@gmail.com>
2021-11-28 19:23:22 -0500

    fixed/added tests to TestSeatFactory and TestSeatManager

--- src/test/java/TestSeatFactory.java
+++ src/test/java/TestSeatFactory.java
@@ -31,123 +31,67 @@ public class TestSeatFactory {
     }
 
     @Test
-    public void testCreateSeatMapId() {
-        ArrayList<Seat> seatMap = seatFactory.createSeatMap(3, 3, 3);
+    public void testCreateSeatMap() {
+        ArrayList<Seat> seatMap = seatFactory.createSeatMap(252, 90, 18);
 
-        ArrayList<Seat> expectedSeatMap = new ArrayList<>();
+        assertEquals(360, seatMap.size());
 
-        expectedSeatMap.add(new EconomySeat(0, 100));
-        expectedSeatMap.add(new EconomySeat(1, 100));
-        expectedSeatMap.add(new EconomySeat(2, 100));
-        expectedSeatMap.add(new BusinessClassSeat(3, 150));
-        expectedSeatMap.add(new BusinessClassSeat(4, 150));
-        expectedSeatMap.add(new BusinessClassSeat(5, 150));
-        expectedSeatMap.add(new FirstClassSeat(6, 200));
-        expectedSeatMap.add(new FirstClassSeat(7, 200));
-        expectedSeatMap.add(new FirstClassSeat(8, 200));
-
-        assertEquals(seatMap.get(0).getId(), expectedSeatMap.get(0).getId());
-        assertEquals(seatMap.get(1).getId(), expectedSeatMap.get(1).getId());
-        assertEquals(seatMap.get(2).getId(), expectedSeatMap.get(2).getId());
-        assertEquals(seatMap.get(3).getId(), expectedSeatMap.get(3).getId());
-        assertEquals(seatMap.get(4).getId(), expectedSeatMap.get(4).getId());
-        assertEquals(seatMap.get(5).getId(), expectedSeatMap.get(5).getId());
-        assertEquals(seatMap.get(6).getId(), expectedSeatMap.get(6).getId());
-        assertEquals(seatMap.get(7).getId(), expectedSeatMap.get(7).getId());
-        assertEquals(seatMap.get(8).getId(), expectedSeatMap.get(8).getId());
-
+        for (int i = 0; i < seatMap.size(); i++) {
+            if (i < 252) {
+                assertEquals("Economy", seatMap.get(i).getSeatClass());
+                assertEquals(100, seatMap.get(i).getPrice(), 0);
+                assertEquals(i, seatMap.get(i).getId());
+            } else if (i < 342) {
+                assertEquals("Business", seatMap.get(i).getSeatClass());
+                assertEquals(150, seatMap.get(i).getPrice(), 0);
+                assertEquals(i, seatMap.get(i).getId());
+            } else {
+                assertEquals("First", seatMap.get(i).getSeatClass());
+                assertEquals(200, seatMap.get(i).getPrice(), 0);
+                assertEquals(i, seatMap.get(i).getId());
+            }
+        }
     }
 
     @Test
-    public void testCreateSeatMapPrice() {
-        ArrayList<Seat> seatMap = seatFactory.createSeatMap(3, 3, 3);
+    public void testGetSeatsOfClassEconomy() {
+        ArrayList<Seat> seatMap = seatFactory.createSeatMap(252, 90, 18);
+        ArrayList<Seat> economy = seatFactory.getSeatsOfClass(seatMap, "1");
 
-        ArrayList<Seat> expectedSeatMap = new ArrayList<>();
+        assertEquals(252, economy.size());
 
-        expectedSeatMap.add(new EconomySeat(0, 100));
-        expectedSeatMap.add(new EconomySeat(1, 100));
-        expectedSeatMap.add(new EconomySeat(2, 100));
-        expectedSeatMap.add(new BusinessClassSeat(3, 150));
-        expectedSeatMap.add(new BusinessClassSeat(4, 150));
-        expectedSeatMap.add(new BusinessClassSeat(5, 150));
-        expectedSeatMap.add(new FirstClassSeat(6, 200));
-        expectedSeatMap.add(new FirstClassSeat(7, 200));
-        expectedSeatMap.add(new FirstClassSeat(8, 200));
-
-        assertEquals(seatMap.get(0).getPrice(), expectedSeatMap.get(0).getPrice(), 0);
-        assertEquals(seatMap.get(1).getPrice(), expectedSeatMap.get(1).getPrice(), 0);
-        assertEquals(seatMap.get(2).getPrice(), expectedSeatMap.get(2).getPrice(), 0);
-        assertEquals(seatMap.get(3).getPrice(), expectedSeatMap.get(3).getPrice(), 0);
-        assertEquals(seatMap.get(4).getPrice(), expectedSeatMap.get(4).getPrice(), 0);
-        assertEquals(seatMap.get(5).getPrice(), expectedSeatMap.get(5).getPrice(), 0);
-        assertEquals(seatMap.get(6).getPrice(), expectedSeatMap.get(6).getPrice(), 0);
-        assertEquals(seatMap.get(7).getPrice(), expectedSeatMap.get(7).getPrice(), 0);
-        assertEquals(seatMap.get(8).getPrice(), expectedSeatMap.get(8).getPrice(), 0);
+        for (int i = 0; i < economy.size(); i++) {
+            assertEquals("Economy", economy.get(i).getSeatClass());
+            assertEquals(100, economy.get(i).getPrice(), 0);
+            assertEquals(i, economy.get(i).getId());
+        }
     }
 
     @Test
-    public void testCreateSeatMapType() {
-        ArrayList<Seat> seatMap = seatFactory.createSeatMap(3, 3, 3);
+    public void testGetSeatsOfClassBusiness() {
+        ArrayList<Seat> seatMap = seatFactory.createSeatMap(252, 90, 18);
+        ArrayList<Seat> businessClass = seatFactory.getSeatsOfClass(seatMap, "2");
 
-        ArrayList<Seat> expectedSeatMap = new ArrayList<>();
+        assertEquals(90, businessClass.size());
 
-        expectedSeatMap.add(new EconomySeat(0, 100));
-        expectedSeatMap.add(new EconomySeat(1, 100));
-        expectedSeatMap.add(new EconomySeat(2, 100));
-        expectedSeatMap.add(new BusinessClassSeat(3, 150));
-        expectedSeatMap.add(new BusinessClassSeat(4, 150));
-        expectedSeatMap.add(new BusinessClassSeat(5, 150));
-        expectedSeatMap.add(new FirstClassSeat(6, 200));
-        expectedSeatMap.add(new FirstClassSeat(7, 200));
-        expectedSeatMap.add(new FirstClassSeat(8, 200));
-
-        assertEquals(seatMap.get(0).getId(), expectedSeatMap.get(0).getId());
-        assertEquals(seatMap.get(1).getId(), expectedSeatMap.get(1).getId());
-        assertEquals(seatMap.get(2).getId(), expectedSeatMap.get(2).getId());
-        assertEquals(seatMap.get(3).getId(), expectedSeatMap.get(3).getId());
-        assertEquals(seatMap.get(4).getId(), expectedSeatMap.get(4).getId());
-        assertEquals(seatMap.get(5).getId(), expectedSeatMap.get(5).getId());
-        assertEquals(seatMap.get(6).getId(), expectedSeatMap.get(6).getId());
-        assertEquals(seatMap.get(7).getId(), expectedSeatMap.get(7).getId());
-        assertEquals(seatMap.get(8).getId(), expectedSeatMap.get(8).getId());
+        for (int i = 0; i < businessClass.size(); i++) {
+            assertEquals("Business", businessClass.get(i).getSeatClass());
+            assertEquals(150, businessClass.get(i).getPrice(), 0);
+            assertEquals(i + 252, businessClass.get(i).getId());
+        }
     }
 
     @Test
-    public void testCreateSeatMapLength() {
-        ArrayList<Seat> seatMap = seatFactory.createSeatMap(3, 3, 3);
-        assertEquals(9, seatMap.size());
+    public void testGetSeatsOfClassFirst() {
+        ArrayList<Seat> seatMap = seatFactory.createSeatMap(252, 90, 18);
+        ArrayList<Seat> firstClass = seatFactory.getSeatsOfClass(seatMap, "3");
+
+        assertEquals(18, firstClass.size());
+
+        for (int i = 0; i < firstClass.size(); i++) {
+            assertEquals("First", firstClass.get(i).getSeatClass());
+            assertEquals(200, firstClass.get(i).getPrice(), 0);
+            assertEquals(i + 252 + 90, firstClass.get(i).getId());
+        }
     }
-
-
-//    This implementation of the test cannot work since getSeatOfClass relies on an arraylist
-//    of seats of size 358 (252 economy seats, 90 business class seats, 18 first class seats)
-//    representing an airplane. These values can be found in the Flight entity class.
-//    This implementation would only work on a list of 9 seats which is not possible since we
-//    require 358 for the GetSeatsOfClass function
-//    Due to this size, it would force us to create an arraylist of seats of size 358 to make tests upon
-//    and get our sub-lists of different types of seats. This task is tedious and time-consuming,
-//    but we are confident our function works.
-
-//    @Test
-//    public void testGetSeatsOfClassEconomy() {
-//        ArrayList<Seat> seatMap = seatFactory.createSeatMap(3, 3, 3);
-//        ArrayList<Seat> economy = seatFactory.getSeatsOfClass(seatMap, "1");
-//
-//        ArrayList<Seat> expectedSeatMap = new ArrayList<>();
-//        expectedSeatMap.add(new EconomySeat(0, 100));
-//        expectedSeatMap.add(new EconomySeat(1, 100));
-//        expectedSeatMap.add(new EconomySeat(2, 100));
-//
-//        // compare price's and id's
-//        assertEquals(economy.get(0).getId(), expectedSeatMap.get(0).getId());
-//        assertEquals(economy.get(0).getPrice(), expectedSeatMap.get(0).getPrice(), 0);
-//        assertEquals(economy.get(1).getId(), expectedSeatMap.get(1).getId());
-//        assertEquals(economy.get(1).getPrice(), expectedSeatMap.get(1).getPrice(), 0);
-//        assertEquals(economy.get(2).getId(), expectedSeatMap.get(2).getId());
-//        assertEquals(economy.get(2).getPrice(), expectedSeatMap.get(2).getPrice(), 0);
-//
-//        //compare size of array list
-//        assertEquals(economy.size(), expectedSeatMap.size());
-//
-//    }
 }
--- src/test/java/TestSeatManager.java
+++ src/test/java/TestSeatManager.java
@@ -17,23 +17,23 @@ public class TestSeatManager {
     @Test
     public void testCalculateRefundByDaysLeft() {
         double full_refund = seatManager.calculateRefundByDaysLeft(7, 28);
-        assertEquals(200, full_refund);
+        assertEquals(200, full_refund, 0);
 
         double partial_refund = seatManager.calculateRefundByDaysLeft(2, 28);
-        assertEquals(148, partial_refund);
+        assertEquals(148, partial_refund, 0);
 
         double no_refund = seatManager.calculateRefundByDaysLeft(0, 28);
-        assertEquals(0, no_refund);
+        assertEquals(0, no_refund, 0);
 
     }
 
     @Test
     public void testCalculateDateChangeChargeByDateLeft() {
         double no_charge = seatManager.calculateDateChangeChargeByDateLeft(7, 28);
-        assertEquals(0, no_charge);
+        assertEquals(0, no_charge, 0);
 
         // charged $50 if date is changed 3 days before the departure date
         double charge = seatManager.calculateDateChangeChargeByDateLeft(3, 28);
-        assertEquals(50, charge);
+        assertEquals(50, charge, 0);
     }
 }

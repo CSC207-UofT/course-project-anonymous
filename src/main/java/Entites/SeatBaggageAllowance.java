5072f4e1a7887f18c9f4140260c1fee40ab6cb0b
dinkarverma02 <dinkarverma2002@gmail.com>
2021-11-30 00:55:13 -0500

    added javadoc to decorator classes

--- src/main/java/Entites/SeatBaggageAllowance.java
+++ src/main/java/Entites/SeatBaggageAllowance.java
@@ -2,7 +2,13 @@ package Entites;
 
 public interface SeatBaggageAllowance {
 
+    /**
+     * @return the Seat's number of cabin bags allowed
+     **/
     int numberOfCabinBagsAllowed();
 
+    /**
+     * @return the Seat's number of check in bags allowed
+     **/
     int numberOfCheckInBagsAllowed();
 }
--- src/main/java/UseCases/decorators/SeatDecorator.java
+++ src/main/java/UseCases/decorators/SeatDecorator.java
@@ -1,18 +1,29 @@
 package UseCases.decorators;
-
 import Entites.*;
 
 public abstract class SeatDecorator implements SeatBaggageAllowance{
     protected Seat decoratedSeat;
 
+    /**
+     * Construct a SeatDecorator, giving it the given
+     * Seat object.
+     *
+     * @param decoratedSeat A seat object
+     */
     public SeatDecorator(Seat decoratedSeat){
         this.decoratedSeat = decoratedSeat;
     }
 
+    /**
+     * @return the number of cabin bags allowed
+     */
     public int numberOfCabinBagsAllowed(){
         return decoratedSeat.numberOfCabinBagsAllowed();
     }
 
+    /**
+     * @return the number of check in bags allowed
+     */
     public int numberOfCheckInBagsAllowed(){
         return decoratedSeat.numberOfCheckInBagsAllowed();
     }

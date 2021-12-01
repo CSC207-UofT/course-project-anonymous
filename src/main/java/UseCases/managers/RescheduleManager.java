1e9a0efb395b9b15586636ada84780b565feb4e8
Avnish Pasari <avnish.pasari@mail.utoronto.ca>
2021-12-01 18:25:35 -0500

    Added missing docstring to the Reschedule Manager class

--- src/main/java/UseCases/managers/RescheduleManager.java
+++ src/main/java/UseCases/managers/RescheduleManager.java
@@ -6,6 +6,12 @@ import Entites.Ticket;
 
 public class RescheduleManager {
     public void reschedule(Ticket ticket, Flight flight, TicketManager ticketManager) {
+        /**
+         * Reschedule the ticket of a passanger
+         * @param ticket input the old ticket details of the passanger
+         * @param flight input the new flight details
+         * @param ticketManager object to call functions to help reschedule the ticket to a new ticket
+         */
 
         Seat oldSeat = ticket.getSeat();
         int seatIndex = ticket.getFlight().getSeatNo(oldSeat);

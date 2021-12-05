da595c3d7317973abdc41ef081d12132a94bc109
Avnish Pasari <avnish.pasari@mail.utoronto.ca>
2021-12-04 19:33:38 -0500

    Made changes to the class to pass the test which it was failing in Phase1.
    Added more tests to test SeatManager more extensively.
    
    Added missing javadoc.

--- src/test/java/TestSeatManager.java
+++ src/test/java/TestSeatManager.java
@@ -16,24 +16,37 @@ public class TestSeatManager {
 
     @Test
     public void testCalculateRefundByDaysLeft() {
+        // Given full refund if ticket is cancelled 7 days before departure
         double full_refund = seatManager.calculateRefundByDaysLeft(7, 28);
-        assertEquals(200, full_refund);
+        assertEquals(200.0, full_refund,0);
 
+        // Given partial refund of $148 if ticket is cancelled 2 days before departure
         double partial_refund = seatManager.calculateRefundByDaysLeft(2, 28);
-        assertEquals(148, partial_refund);
+        assertEquals(148.0, partial_refund,0);
 
+        // Given partial refund of $180 if ticket is cancelled 5 days before departure
+        double partial_refund2 = seatManager.calculateRefundByDaysLeft(5, 15);
+        System.out.println(partial_refund2);
+        assertEquals(180.0, partial_refund2,0);
+
+        // Given no refund if ticket is cancelled just before departure
         double no_refund = seatManager.calculateRefundByDaysLeft(0, 28);
-        assertEquals(0, no_refund);
+        assertEquals(0.0, no_refund,0);
 
     }
 
     @Test
     public void testCalculateDateChangeChargeByDateLeft() {
+        // charged $0 if date is changed 7 days before the departure date
         double no_charge = seatManager.calculateDateChangeChargeByDateLeft(7, 28);
-        assertEquals(0, no_charge);
+        assertEquals(0.0, no_charge,0);
+
+        // charged $46 if date is changed 5 days before the departure date
+        double charge1 = seatManager.calculateDateChangeChargeByDateLeft(5, 28);
+        assertEquals(46.0, charge1,0);
 
         // charged $50 if date is changed 3 days before the departure date
         double charge = seatManager.calculateDateChangeChargeByDateLeft(3, 28);
-        assertEquals(50, charge);
+        assertEquals(50.0, charge,0);
     }
 }

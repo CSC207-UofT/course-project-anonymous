00e1de98d18975e8392e39d51d3b2fe06ba76df5
Avnish Pasari <avnish.pasari@mail.utoronto.ca>
2021-12-04 19:39:32 -0500

    Creating the TestTransactionManager class to test the Transaction Manager Use Case

--- src/test/java/TestTransactionManager.java
+++ src/test/java/TestTransactionManager.java
@@ -35,21 +35,24 @@ public class TestTransactionManager {
     Baggage checked_in3;
     Ticket ticket1;
     Flight flight1;
-    AirlinesManager airlinesManager;
+    AirlinesManager airlinesManager = new AirlinesManager();
     LocalDateTime date3 = LocalDateTime.of(2020, 05, 1, 14, 30);
     LocalDateTime date4 = LocalDateTime.of(2020, 05, 10, 18, 30);
 
     @Before
     public void initializeManager(){
+        /**
+         * Initializing the data members and putting some generated/default values in them (for testing purposes).
+         */
         transactionManager = new TransactionManager();
         passenger = new Passenger(123,"Avnish", "abcd@mail.com","12436");
         seat = new EconomySeat(111, 200);
         meal = new Meal("Cake",10.2,13.3,true);
         cabin1 = new CabinBaggage(12.1,11.1,32.0);
         cabin2 = new CabinBaggage(10.2,3.3,4.3);
-        checked_in1 = new CabinBaggage(13.4,5.3,15.8);
-        checked_in2 = new CabinBaggage(5.5,4.5,11.3);
-        checked_in3 = new CabinBaggage(7.4,6.3,10.9);
+        checked_in1 = new CheckInBaggage(13.4,5.3,15.8);
+        checked_in2 = new CheckInBaggage(5.5,4.5,11.3);
+        checked_in3 = new CheckInBaggage(7.4,6.3,10.9);
         baggages.add(cabin1);
         baggages.add(cabin2);
         baggages.add(checked_in1);
@@ -57,23 +60,28 @@ public class TestTransactionManager {
         baggages.add(checked_in3);
         airlinesManager.addAirline("Air India");
         flight1 = new Flight(date3, date4,"Delhi","Toronto",10.5, airlinesManager.getAirline("Air India"));
-        ticket1 = new Ticket(passenger, flight1, seat,false);
+        ticket1 = new Ticket(passenger, flight1, flight1.getSeatAtIndex(1), false);
+        ticket1.setBaggages(new ArrayList<>());
+        ticket1.setMeal(meal);
 
     }
 
     @Test
     public void testTransactionManager()
     {
+        // Testing the createTransactionForNewTicket method
         Transaction test1 = transactionManager.createTransactionForNewTicket(passenger, seat, meal, baggages);
         assert(test1 instanceof Transaction);
 
+        // Testing the createTransactionRefundTicket method
         Transaction test2 = transactionManager.createTransactionRefundTicket(ticket1);
         assert(test2 instanceof Transaction);
 
+        // Testing the createTransactionRescheduleTicket method
         Transaction test3 = transactionManager.createTransactionRescheduleTicket(ticket1, flight1);
         assert(test3 instanceof Transaction);
 
-        transactionManager.addTransaction(test1);
+        // Testing that a ticket is removed successfully from the system
         transactionManager.remove(test1);
 
     }

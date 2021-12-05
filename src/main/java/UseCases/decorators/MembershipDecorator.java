99d2bddb8462c781e4f38f7b53bcd2aac9d049e5
dinkarverma02 <dinkarverma2002@gmail.com>
2021-12-04 19:32:41 -0500

    partially implemented decorator design pattern for Seat. Rest will be done by Avnish, including testing.

---
+++ src/main/java/UseCases/decorators/MembershipDecorator.java
@@ -0,0 +1,59 @@
+package UseCases.decorators;
+import Entites.*;
+
+public abstract class MembershipDecorator implements MembershipStatus {
+    protected MembershipStatus membershipStatus;
+
+    /**
+     * Construct a MembershipDecorator, giving it the given
+     * MembershipStatus object.
+     *
+     * @param membershipStatus A MembershipStatus object
+     */
+    public MembershipDecorator(MembershipStatus membershipStatus){
+        this.membershipStatus = membershipStatus;
+    }
+
+    /**
+     * return the price of the flight after a discount
+     * is applied
+     *
+     * @param price the flight's price
+     *
+     * @return the flight's discounted price
+     **/
+    public double getFlightDiscount(double price) {
+        return membershipStatus.getFlightDiscount(price);
+    }
+
+    /**
+     * return the price of the meal after a discount
+     * is applied
+     *
+     * @param price the meal's price
+     *
+     * @return the meal's discounted price
+     **/
+    public double getMealDiscount(double price) {
+        return membershipStatus.getMealDiscount(price);
+    }
+
+    /**
+     * return the price of extra baggage after a discount
+     * is applied
+     *
+     * @param price the extra baggage price
+     *
+     * @return the extra baggage discounted price
+     **/
+    public double getExtraBaggageDiscount(double price) {
+        return membershipStatus.getExtraBaggageDiscount(price);
+    }
+
+    /**
+     * @return the name of the membership
+     */
+    public String getMembershipName() {
+        return membershipStatus.getMembershipName();
+    }
+}

fa02cd1782ea3e09be373c05a243c576682d7774
Avnish Pasari <avnish.pasari@mail.utoronto.ca>
2021-12-05 19:05:31 -0500

    Added factories to map the information of entities so that the upper classes (the command UI) are not directly dependent on the entities.

---
+++ src/main/java/UseCases/factories/BaggageFactory.java
@@ -0,0 +1,42 @@
+package UseCases.factories;
+
+import Entites.Baggage;
+import Entites.CabinBaggage;
+import Entites.CheckInBaggage;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class BaggageFactory {
+    public ArrayList<Baggage> createBaggageList(ArrayList<Map<String, Double>> baggagesInfo) {
+        ArrayList<Baggage> baggages = new ArrayList<>();
+
+        for (Map<String, Double> baggageInfo : baggagesInfo) {
+            double weight = baggageInfo.get("weight");
+            double width = baggageInfo.get("width");
+            double height = baggageInfo.get("height");
+
+            if (Double.compare(baggageInfo.get("type") ,0.0) == 0) {
+                baggages.add(new CabinBaggage(weight, width, height));
+            } else {
+                baggages.add(new CheckInBaggage(weight, width, height));
+            }
+        }
+
+        return baggages;
+    }
+
+    public ArrayList<Baggage> createBaggagesForDataLoading(int noOfCabinBags, int noOfCheckInBags) {
+        ArrayList<Baggage> baggages = new ArrayList<>();
+
+        for (int i = 0; i < noOfCabinBags; i++) {
+            baggages.add(new CabinBaggage(6, 7, 7));
+        }
+
+        for (int i = 0; i < noOfCheckInBags; i++) {
+            baggages.add(new CheckInBaggage(20, 23, 23));
+        }
+
+        return baggages;
+    }
+}

300c6f10e703aabb4b7c6087779494faf153e67d
Dixshant Shree Shreemal <dixshant.shree@mail.utoronto.ca>
2021-12-05 22:11:23 -0500

    Docstrings for gateway

--- src/main/java/interfaceAdapter/gateway/PassengerSessionHandler.java
+++ src/main/java/interfaceAdapter/gateway/PassengerSessionHandler.java
@@ -4,11 +4,17 @@ import Entites.Passenger;
 import interfaceAdapter.controller.*;
 
 public class PassengerSessionHandler extends UserSessionHandler {
+    /**
+     * A subclass of the abstract UserSessionHandler. This class
+     * acts as a gateway between the UI and the presenters/controllers
+     * relay the information
+     */
     public Passenger passenger;
     public BookingSystem bookingSystem;
     public DatabaseConnector databaseConnector;
     public PassengerDataHandler passengerDataHandler;
     public TicketDataHandler ticketDataHandler;
+
 
     public PassengerSessionHandler() {
         bookingSystem = new BookingSystem();
@@ -23,8 +29,14 @@ public class PassengerSessionHandler extends UserSessionHandler {
         this.bookingSystem.ticketManager.addObserver(ticketDataHandler);
     }
 
+    /**
+     * implements the abstract method from parent class
+     * @param id the id of the passenger
+     * @return returns a boolean depedning on whether a passenger with this ID exists
+     */
     @Override
     public boolean setSessionUserWithId(int id) {
+
         this.passenger = this.bookingSystem.passengerManager.getPassengerWithId(id);
         return !this.passenger.equals(null);
     }
